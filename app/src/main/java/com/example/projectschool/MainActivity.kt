2b97b63e36d926215c64eb12394c878bea68012a
kimjw2003 <kimjw111003@gmail.com>
2020-09-21 19:50:05 +0900

    [ADD] sentenceText, if로직

--- app/src/main/java/com/example/projectschool/MainActivity.kt
+++ app/src/main/java/com/example/projectschool/MainActivity.kt
@@ -19,21 +19,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getCurrentWeather()
-    }
 
+    }
 
     fun getCurrentWeather() {
 
         RetrofitClient.retrofitService.getCurrentWeather(
             "7UhrTwPM239CrHxGYdXDboLOSh7OmX65p12WEAaev2FNpUNCSPddYen1%2Fjh0VnQyQzXzg5nOwLlOxXPkRuggFQ%3D%3D",
-            "10", "1", "JSON", "11A00101"
+            "10", "1", "JSON", "11H10604"
         ).enqueue(object : Callback<Base>{
             override fun onFailure(call: Call<Base>, t: Throwable) {
                 Log.d("Logg", t.message)
             }
 
             override fun onResponse(call: Call<Base>, response: Response<Base>) {
-                 text1.text = response.body()?.response!!.body!!.items.item[1].rnSt.toString()
+                 text1.text = response.body()?.response!!.body!!.items.item[1].rnSt.toString()+"%"
+                if(text1.text == "30%" || text1.text == "20%" || text1.text == "10%" || text1.text == "0%") {
+                    sentence.text="아침점호 안나가긴 글렀네요.."
+                }else if(text1.text == "40%" || text1.text == "50%" || text1.text == "60%"){
+                    sentence.text="희망이 있어요~!"
+                }else{
+                    sentence.text="내일은 꿀잠 자겠네요 ㅎㅎ"
+                }
             }
         })
 
