c1d908f76c01f366cdf3c6713c4bd47f90dbdd8c
kimjw2003 <kimjw111003@gmail.com>
2020-09-22 17:34:53 +0900

    [ADD] Temp & TempSentence

--- app/src/main/java/com/example/projectschool/MainActivity.kt
+++ app/src/main/java/com/example/projectschool/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.projectschool
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -34,21 +35,32 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call<Base>, response: Response<Base>) {
 
-                 text1.text = response.body()?.response!!.body!!.items.item[1].rnSt.toString()+"%"
-                if(text1.text == "30%" || text1.text == "20%" || text1.text == "10%" || text1.text == "0%") {
-                    sentence.text="아침점호 안나가긴 글렀네요.."
-                }else if(text1.text == "40%" || text1.text == "50%" || text1.text == "60%"){
-                    sentence.text="희망이 있어요~!"
+                text1.text = response.body()?.response!!.body!!.items.item[1].rnSt.toString() + "%"
+                if (text1.text == "30%" || text1.text == "20%" || text1.text == "10%" || text1.text == "0%") {
+                    sentence.text = "아침점호 안나가긴 글렀네요.."
+                } else if (text1.text == "40%" || text1.text == "50%" || text1.text == "60%") {
+                    sentence.text = "희망이 있어요~!"
                     imageView.setImageResource(R.drawable.soso)
-                }else{
-                    sentence.text="내일은 꿀잠 자겠네요 ㅎㅎ"
+                } else {
+                    sentence.text = "내일은 꿀잠 자겠네요 ㅎㅎ"
                     imageView.setImageResource(R.drawable.happy)
                 }
 
-                text2.text = response.body()?.response!!.body!!.items.item[1].ta.toString()+"ºC"
+                text2.text =
+                    response.body()?.response?.body?.items?.item?.get(1)?.ta.toString()
+                if (text2.text.toString().toInt() < 10) {
+                    sentence2.text = "날씨가 굉장히 추워요"
+                    sentence2.setTextColor(Color.parseColor("#0080f0"))
+                } else if (text2.text.toString().toInt() in 10..19) {
+                    sentence2.text = "날씨가 쌀쌀해요"
+                    sentence2.setTextColor(Color.parseColor("#50BCDF"))
+                } else if (text2.text.toString().toInt() in 20..29) {
+                    sentence2.text = "날씨가 따뜻해요"
+                    sentence2.setTextColor(Color.parseColor("#FF7F00"))
+                } else {
 
+                }
             }
         })
-
     }
 }
