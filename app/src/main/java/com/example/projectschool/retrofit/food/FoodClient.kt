4290ba74bedf171e40138172ebc220192f831684
kimjw2003 <kimjw111003@gmail.com>
2020-09-24 13:20:44 +0900

    ADD Dao

--- app/src/main/java/com/example/projectschool/retrofit/food/FoodClient.kt
+++ app/src/main/java/com/example/projectschool/retrofit/food/FoodClient.kt
@@ -1,4 +1,29 @@
 package com.example.projectschool.retrofit.food
 
+import com.example.projectschool.retrofit.weather.WeatherDao
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 object FoodClient {
+    var retrofitService: FoodDao
+
+    init {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val logger = OkHttpClient.Builder().addInterceptor(interceptor)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS).build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(logger)
+            .build()
+
+        retrofitService = retrofit.create(FoodDao::class.java)
+    }
 }
--- app/src/main/java/com/example/projectschool/retrofit/food/FoodDao.kt
+++ app/src/main/java/com/example/projectschool/retrofit/food/FoodDao.kt
@@ -1,4 +1,17 @@
 package com.example.projectschool.retrofit.food
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface FoodDao {
+    @GET("food")
+    fun getTomorrowFood(
+        @Query("foodkey", encoded = true) foodkey : String,
+        @Query("dataType", encoded = true) dataType : String,
+        @Query("pIndex", encoded = true) pIndex : Int,
+        @Query("pSize", encoded = true) pSize: Int,
+        @Query("ATPT_OFCDC_SC_CODE", encoded = true) ATPT_OFCDC_SC_CODE : String,
+        @Query("SD_SCHUL_CODE", encoded = true) SD_SCHUL_CODE : String,
+        @Query("MLSV_YMD", encoded = true) MLSV_YMD : String
+    )
 }
