dd111529de0ef4acb80db8d3513a400e2b871ba2
kimjw2003 <kimjw111003@gmail.com>
2020-09-29 10:17:37 +0900

    [FIX] error

--- app/src/main/java/com/example/projectschool/fragment/TimeFragment.kt
+++ app/src/main/java/com/example/projectschool/fragment/TimeFragment.kt
@@ -11,7 +11,7 @@ import com.example.projectschool.R
 class TimeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_food, container, false)
+        val view = inflater.inflate(R.layout.fragment_time, container, false)
 
         return view
     }
