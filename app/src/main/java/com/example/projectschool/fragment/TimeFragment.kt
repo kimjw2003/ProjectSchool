30fe5cc2602c51c802e29048b57d3b34c8efbdf6
kimjw2003 <kimjw111003@gmail.com>
2020-10-14 14:24:48 +0900

    [UPDATE] 시간표 위치

--- app/src/main/java/com/example/projectschool/fragment/TimeFragment.kt
+++ app/src/main/java/com/example/projectschool/fragment/TimeFragment.kt
@@ -1,17 +1,13 @@
 package com.example.projectschool.fragment
 
-import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.inflate
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import com.example.projectschool.R
 import com.example.projectschool.data.TimeBase
